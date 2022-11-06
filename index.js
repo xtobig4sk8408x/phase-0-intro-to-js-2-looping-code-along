36c11b843ceabc5602e7763e92cebc1407fe1800
Theodore John Josef <john.josef@aol.com>
2022-11-06 01:24:17 -0700

    commit tree 4d27dc284a561ab33ead1e832f229e28239f2333

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let greeting = [];
+    for(let i = 0; i < name.length; i++){
+    greeting.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return greeting;
+}
+
+function countDown()
+{
+    let i = 10;
+    while (i >= 0)
+    {
+        console.log(i--);
+    }
+
+}
